b4e6f212dbf86ee5ae17ac4e43a2b5f5a250c74d
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-09-03 08:57:36 +0300

    commit tree f3e43d4de1e05eaa27f2c87585335bf4c0fd8605

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,47 @@
 class Shop < ActiveRecord::Base
 
+
+    def self.create_with_title(title)
+        Shop.create(title: "This is a title.")
+    end
+
+    def self.first_shop
+        Shop.first
+    end
+
+    def self.last_shop
+        Shop.last
+    end
+
+    def self.shop_count
+        Shop.count
+    end
+
+    def self.find_shop_with_id(id)
+        Shop.find(id)
+    end
+
+    def self.find_shop_with_attributes(title)
+        Shop.find_by(title: title)
+    end
+
+    def self.find_shops_after_2002
+        Shop.where("opening_date > ?", 2002)
+    end
+
+    def update_with_attributes(title)
+        self.update(title: title)
+    end
+
+    def self.update_all_titles(title)
+        Shop.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        Shop.find(id).destroy
+    end
+
+    def self.delete_all_shops
+        Shop.destroy_all
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@ Shop.create(title: "Cosmetics", opening_date: 1983, owner: "John Badham")
 
 Shop.create(title: "Electronics", opening_date: 1983, owner: "John Doe")
 
+Shop.create(title: "Books", opening_date: 2004, owner: "Jane Doe")
+
